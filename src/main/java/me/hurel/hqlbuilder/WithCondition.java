f5464e2d94c70397c976ffed377397544f23719e
nathan <nhurel@gmail.com>
2014-03-02 21:19:45 +0100

    Added copyright on all files

--- src/main/java/me/hurel/hqlbuilder/WithCondition.java
+++ src/main/java/me/hurel/hqlbuilder/WithCondition.java
@@ -1,3 +1,11 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
+ * If a copy of the MPL was not distributed with this file, 
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Contributors:
+ *     Nathan Hurel - initial API and implementation
+ */
 package me.hurel.hqlbuilder;
 
 public interface WithCondition<T> extends Condition<T>, AbstractFromClause {
