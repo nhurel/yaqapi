18a393e4f2aa8b11d8b708577d440b77cbf41fdb
nathan <nhurel@gmail.com>
2014-03-23 20:18:46 +0100

    Added alias support in SELECT clause

--- src/main/java/me/hurel/hqlbuilder/ChainableSelectClause.java
+++ src/main/java/me/hurel/hqlbuilder/ChainableSelectClause.java
@@ -1,8 +1,13 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Contributors:
+ *     Nathan Hurel - initial API and implementation
+ */
 package me.hurel.hqlbuilder;
 
-/**
- * Created by nathan on 22/03/14.
- */
 public interface ChainableSelectClause extends SelectClause {
 
     public AliasableSelectClause andSelect(Object methodCall);
